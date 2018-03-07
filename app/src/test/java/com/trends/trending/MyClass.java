6c557c67bc4f8cd5ba6d99275958cc347693d588
Neeraj <neeraj.vr16@gmail.com>
2018-03-07 22:58:53 +0530

    Initial commit

---
+++ app/src/test/java/com/trends/trending/MyClass.java
@@ -0,0 +1,17 @@
+package com.trends.trending;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by USER on 3/4/2018.
+ */
+
+public class MyClass {
+
+    @Test
+    public void sub(){
+        assertEquals(4, 10-6);
+    }
+}
